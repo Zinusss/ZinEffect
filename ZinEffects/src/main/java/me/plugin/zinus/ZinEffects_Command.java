3b884e8e386b7e1029d242444196045c5f889ed4
_Zinus_ <159170839+Zinusss@users.noreply.github.com>
2025-03-16 13:32:37 +0200

    ZinEffects_Command.java

--- ZinEffects/src/main/java/me/plugin/zinus/ZinEffects_Command.java
+++ ZinEffects/src/main/java/me/plugin/zinus/ZinEffects_Command.java
@@ -20,37 +20,35 @@ public class ZinEffects_Command implements CommandExecutor {
         if (strings.length < 2) return false;
         Player player =  getPlayer(strings[0]);
         Player sender = (Player) commandSender;
-
-
         String effect = strings[1];
-        if (effect.equalsIgnoreCase("bad")){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.BAD_OMEN, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 120, 999));
-            sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
-            commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
+        switch (effect){
+            case "bad":
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.BAD_OMEN, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 120, 999));
+                sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
+                commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
 
-            return true;
-
-        } else if (effect.equalsIgnoreCase("good")) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 120, 999));
-            sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
-            commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
-            return true;
-
-        } else if (effect.equalsIgnoreCase("normal")) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 999));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 120, 999));
-            sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
-            commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
-            return true;
-
+                return true;
+            case "good":
+                player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 120, 999));
+                sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
+                commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
+                return true;
+            case "normal":
+                player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 999));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 120, 999));
+                sender.sendTitle(ChatColor.GREEN + " ☑ Успешно","",10,40,10);
+                commandSender.sendMessage(ChatColor.GREEN + "☑ Успешное использование команды");
+                return true;
         }
+
         return false;
     }
 }
+
